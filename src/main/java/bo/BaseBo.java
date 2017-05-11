45c566f4e2fa132f464d12441952166e8aeff475
Grass Lin <java-sparrow@users.noreply.github.com>
2017-05-11 21:55:50 +0800

    添加 用户表的 记录映射类 及 建表语句

---
+++ src/main/java/bo/BaseBo.java
@@ -0,0 +1,5 @@
+package main.java.bo;
+
+public class BaseBo {
+
+}
---
+++ src/main/java/bo/UserBo.java
@@ -0,0 +1,58 @@
+package main.java.bo;
+
+import java.sql.Timestamp;
+
+/**
+ * 用户表 记录映射类
+ *
+ */
+public class UserBo extends BaseBo {
+	/**
+	 * 主键id
+	 */
+	private int id;
+	/**
+	 * 用户名
+	 */
+	private String username;
+	/**
+	 * 密码
+	 */
+	private String password;
+	/**
+	 * 创建日期
+	 */
+	private Timestamp createTime;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
+}
